921de6609ae291e2bd80c49a70c28f98a0471b62
flenn <flennnabdou@gmail.com>
2025-04-06 17:23:37 +0100

    Initial commit

---
+++ src/main/java/org/flennn/JackpotDuel.java
@@ -0,0 +1,16 @@
+package org.flennn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JackpotDuel extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
